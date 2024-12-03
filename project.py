67a3e0a0e75b169b8e58950323805e0b210c893a
stanley <stanleytheli@gmail.com>
2024-12-03 10:07:37 -0800

    Added gini coefficient computation

--- project.py
+++ project.py
@@ -3,19 +3,31 @@ import pandas
 
 
 ddi_path = "./cps_00001.xml"
-
 csv_path = "./data.csv"
+
 df = pandas.read_csv(csv_path)
 
 avg_income = df.groupby("YEAR")["FTOTVAL"].mean()
 std_income = df.groupby("YEAR")["FTOTVAL"].std()
 
-# calculate gini income coefficient...?
+def gini(series):
+    total = series.iloc[series.shape[0] - 1]
+    proportions = series/total
+
+    equal_area = 0.5
+    diff_area = 0
+    
+    delta = 1/proportions.shape[0]
+    for i in range(proportions.shape[0]):
+        percentile = (i + 1)/proportions.shape[0]
+        diff_area += delta * (percentile - proportions.iloc[i])
+    
+    return diff_area / equal_area
+
+gini_yearly = df.groupby("YEAR")["CUMULATIVE"].agg(gini)
+
 
 print(f"AVERAGE INCOME: {avg_income}")
 print(f"INCOME STDDEV: {std_income}")
 print(f"INCOME STDDEV/AVG: {std_income/avg_income}")
-
-
-#print(f"DataFrame shape: {df.shape}")
-#print(f"First elements: {df.iloc[0:20, :]}") 
+print(f"GINI: {gini_yearly}")
