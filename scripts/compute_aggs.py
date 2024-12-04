4f413033dbc23627a1a656e470336ed5ae7e41da
stanley <stanleytheli@gmail.com>
2024-12-04 00:24:16 -0800

    Added cps_00005 dataset, updated filepaths. Moved edgroup calculation responsibility from compute_aggs to load_data.

--- scripts/compute_aggs.py
+++ scripts/compute_aggs.py
@@ -18,34 +18,7 @@ std_real_income_ed_path = "./aggregate/std_real_income_ed.csv"
 
 gini_path = "./aggregate/gini.csv"
 
-# Utility functions
-def get_years_schooling(code):
-    # Based on IPUMS USA's HIGRADE variable codes.
-    # completed 1st grade = 1 year of schooling, 2nd grade = 2 years...
-    # does not count anything before 1st grade
-    # stops counting additiona years beyond 8th year of college (20 years of schooling)
-    return max(0, code // 10 - 3)
-
-def get_education_group(code):
-    # Based on IPUMS USA's HIGRADE variable codes.
-    # 0 = No school
-    # 1 = Completed elementary school (1st-5th grade)
-    # 2 = Completed middle school (6th-8th grade)
-    # 3 = Completed high school (9th-12th grade)
-    # 4 = Completed 4 years of college (13-16 yrs school)
-    # 5 = Completed 5+ years of college (17+ yrs school)
-    years = get_years_schooling(code)
-    if years == 0:
-        return 0
-    if 1 <= years <= 5:
-        return 1
-    if 6 <= years <= 8:
-        return 2
-    if 9 <= years <= 12:
-        return 3
-    if 13 <= years <= 16:
-        return 4
-    return 5
+calculate_gini = False
 
 def gini(series):
     # Calculates the Gini Coefficient given a series of cumulative incomes
@@ -65,7 +38,6 @@ def gini(series):
 df = pandas.read_csv(csv_path).set_index("YEAR")
 cpi = pandas.read_csv(cpi_path).set_index("YEAR").iloc[:, 0]
 
-df["EDGROUP"] = df["HIGRADE"].apply(get_education_group)
 groupby_year = df.groupby("YEAR")
 groupby_year_ed = df.groupby(["YEAR", "EDGROUP"])
 
@@ -80,8 +52,6 @@ std_real_income = std_income * cpi
 avg_real_income_ed = avg_income_ed * cpi
 std_real_income_ed = std_income_ed * cpi
 
-gini_yearly = groupby_year["CUMULATIVE"].agg(gini)
-
 # Copying
 avg_income.to_csv(avg_income_path)
 std_income.to_csv(std_income_path)
@@ -93,4 +63,7 @@ std_real_income.to_csv(std_real_income_path)
 avg_real_income_ed.to_csv(avg_real_income_ed_path)
 std_real_income_ed.to_csv(std_real_income_ed_path)
 
-gini_yearly.to_csv(gini_path)
+# CUMULATIVE takes a really long time to calculate 
+if calculate_gini:
+    gini_yearly = groupby_year["CUMULATIVE"].agg(gini)
+    gini_yearly.to_csv(gini_path)    
--- scripts/load_data.py
+++ scripts/load_data.py
@@ -6,11 +6,16 @@ CPS_00001 ranges from [1962 - 1989] and contains data on [EDUCATION] and [FAMILY
 1962 and 1963 incomes are not available in the data.
 CPS_00003 ranges from [1962 - 1999] and contains data on [EDUCATION] and [FAMILY INCOME].
 1962 and 1963 incomes are not available in the data.
+1992 - 1999 educations are not available in the data.
+CPS_00005 ranges from [1962 - 1999] and contains data on [EDUCATION] and [FAMILY INCOME].
+1962 and 1963 incomes are not available in the data.
 """
 
-data_path = "./source/cps_00003.dat"
-ddi_path = "./source/cps_00003.xml"
-csv_path = "./source/cps_00003.csv"
+data_path = "./source/cps_00005.dat"
+ddi_path = "./source/cps_00005.xml"
+csv_path = "./source/cps_00005.csv"
+
+calculate_cumulatives = False
 
 print("Reading DDI")
 ddi = readers.read_ipums_ddi(ddi_path)
@@ -18,11 +23,11 @@ print("Finished reading DDI.\nReading Data")
 df = readers.read_microdata(ddi, data_path)
 print("Finished reading Data.")
 
-# filter out people with invalid income
+# filter out people with invalid income/unknown education
 df = df[df["FTOTVAL"] != 9999999999]
+df = df[df["EDUC"] != 999]
 
-df["CUMULATIVE"] = 0
-
+# Utility functions
 def compute_cumulatives(df, year_title, income_title, cumulative_title):
     # Calculates "cumulative incomes" for each year and adds them onto the DataFrame
     # Cumulative income is the total income of everyone who makes less than you
@@ -47,6 +52,62 @@ def compute_cumulatives(df, year_title, income_title, cumulative_title):
         print(f"{round(i/df.shape[0] * 10000) / 100}% done with computing cumulatives")
     return df
 
-df = compute_cumulatives(df, "YEAR", "FTOTVAL", "CUMULATIVE")
+def get_years_schooling_higrade(code):
+    # Based on IPUMS USA's HIGRADE variable codes.
+    # completed 1st grade = 1 year of schooling, 2nd grade = 2 years...
+    # does not count anything before 1st grade
+    # stops counting additiona years beyond 8th year of college (20 years of schooling)
+    return max(0, code // 10 - 3)
+
+def get_education_group_higrade(code):
+    # Based on IPUMS USA's HIGRADE variable codes.
+    # 0 = No school
+    # 1 = Completed elementary school (1st-5th grade)
+    # 2 = Completed middle school (6th-8th grade)
+    # 3 = Completed high school (9th-12th grade)
+    # 4 = Completed 4 years of college (13-16 yrs school)
+    # 5 = Completed 5+ years of college (17+ yrs school)
+    years = get_years_schooling_higrade(code)
+    if years == 0:
+        return 0
+    if 1 <= years <= 5:
+        return 1
+    if 6 <= years <= 8:
+        return 2
+    if 9 <= years <= 12:
+        return 3
+    if 13 <= years <= 16:
+        return 4
+    return 5
+
+def get_education_group_educ(code):
+    # Based on IPUMS USA's EDUC code. 
+    # 0 = No school
+    # 1 = Completed 1-4 years of school (1st-4th grade)
+    # 2 = Completed 5-8 years of school (5th-8th grade)
+    # 3 = Completed 9-12 years of school (9th-12th grade)
+    #     OR high school diploma/equivalent
+    # 4 = Completed 13-16 years of school (1-4 years college)
+    # 5 = Completed 17+ years of school (5+ years college)
+    if 0 <= code <= 2:
+        return 0
+    if 10 <= code <= 14:
+        return 1
+    if 20 <= code <= 32:
+        return 2
+    if 40 <= code <= 73:
+        return 3
+    if 80 <= code <= 111:
+        return 4
+    if 120 <= code <= 125:
+        return 5
+    if code == 999:
+        return None
+
+if calculate_cumulatives:
+    df["CUMULATIVE"] = 0
+    df = compute_cumulatives(df, "YEAR", "FTOTVAL", "CUMULATIVE")
+
+df["EDGROUP"] = df["EDUC"].apply(get_education_group_educ)
 
 df.to_csv(csv_path, index=False)
